cf84c1090ba4363601ace6d439bdc4e50b972975
mocozairos <mcruzdelirafilho@gmail.com>
2024-09-14 00:11:07 -0300

    Update roteirizador_trf_out_rec.py

--- roteirizador_trf_out_rec.py
+++ roteirizador_trf_out_rec.py
@@ -3713,8 +3713,6 @@ elif roteirizar and servico_roteiro=='OUT (MARAGOGI | JAPARATINGA)':
 
     st.session_state.df_roteiros_apoios_alternativos = df_roteiros_apoios_alternativos
 
-    st.session_state['servico_roteiro'] = servico_roteiro
-
     verificar_rotas_alternativas_ou_plotar_roteiros(df_roteiros_alternativos, row_warning, row3, coluna, df_hoteis_pax_max, 
                                                     df_juncoes_pax_max, df_voos_pax_max, df_router_filtrado_2, df_roteiros_apoios, 
                                                     df_roteiros_apoios_alternativos, st.session_state.df_juncao_voos, 
