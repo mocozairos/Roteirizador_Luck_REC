5baa42d8d66d8fd41055519ad7d7cfe9ee001e33
mocozairos <mcruzdelirafilho@gmail.com>
2024-11-06 19:30:31 -0300

    Update roteirizador_trf_out_rec.py

--- roteirizador_trf_out_rec.py
+++ roteirizador_trf_out_rec.py
@@ -4028,6 +4028,8 @@ st.set_page_config(layout='wide')
 
 st.title('Roteirizador de Transfer Out - Recife')
 
+st.title('Roteirizador de Transfer Out - Recife')
+
 st.divider()
 
 st.header('Parâmetros')
